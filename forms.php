eab98af26d54379974b0ff82c4f0420f8726d240
Teddy Pascual <teddylayson2000@gmail.com>
2023-09-05 08:19:33 +0800

    Post and Get demo

---
+++ forms.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="forms.php" method="get">
+    <label for="text">Input Text: </label>
+    <input type="text" name="text" id="text">
+    <button type="submit">Submit</button>
+    </form>
+    <?php
+        if($_SERVER["REQUEST_METHOD"] == "POST") {
+            $name = $_POST["text"];
+            echo "<h1>$name</h1>";
+        } else {
+            $name = $_GET["text"];
+            echo "<h2>GET $name</h2>";
+        }
+    ?>
+</body>
+</html>
